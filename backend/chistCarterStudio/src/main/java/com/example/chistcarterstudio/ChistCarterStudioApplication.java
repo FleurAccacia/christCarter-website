f416dacb5c1f63776daaddc3feaf033469c98116
Gracia GOKAR <graciajulienne@gmail.com>
2025-04-28 11:07:54 +0000

    dossier backend

---
+++ backend/chistCarterStudio/src/main/java/com/example/chistcarterstudio/ChistCarterStudioApplication.java
@@ -0,0 +1,13 @@
+package com.example.chistcarterstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChistCarterStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChistCarterStudioApplication.class, args);
+	}
+
+}
---
+++ backend/chistCarterStudio/src/test/java/com/example/chistcarterstudio/ChistCarterStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chistcarterstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChistCarterStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
